2a297b4e11b805250a8b9d4e5d33853cbcc23c73
cong.wu <cong.wu@accenture.com>
2018-04-28 13:40:48 +0800

    CHANGE

--- js/article_add.js
+++ js/article_add.js
@@ -46,3 +46,27 @@ function domToimage(table) {
         });
     return deferred.promise();
 }
+
+//传入图片路径，返回base64
+function getBase64(img) {
+    function getBase64Image(img, width, height) {//width、height调用时传入具体像素值，控制大小 ,不传则默认图像大小
+        var canvas = document.createElement("canvas");
+        canvas.width = width ? width : img.width;
+        canvas.height = height ? height : img.height;
+
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+        var dataURL = canvas.toDataURL();
+        return dataURL;
+    }
+    var image = new Image();
+    image.crossOrigin = '';
+    image.src = img;
+    var deferred = $.Deferred();
+    if (img) {
+        image.onload = function () {
+            deferred.resolve(getBase64Image(image));//将base64传给done上传处理
+        };
+        return deferred.promise();//问题要让onload完成后再return sessionStorage['imgTest']
+    }
+}
